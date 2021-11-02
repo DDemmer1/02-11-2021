31f445b82fd5798f53e520f4c59873f93773bde1
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2021-11-02 11:10:48 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -2,8 +2,32 @@ package de.demmer.dennis;
 
 public class Application {
 	
+	
 	public static void main(String[] args) {
+	
+
+		
+		Smartphone s1 = new Smartphone();
+		Smartphone s2 = new Smartphone();
+		Smartphone s3 = new Smartphone();
+//		
+//		
+		s1.color = "schwarz";
+		s2.color = "blau";
+		s3.color = "grau";
+		
+		s1.model = "Samsung s20";
+		s2.model = "huawei p20 pro";
+		s3.model = "OnePlus 7t";
+		
+		Smartphone.osVersion = 5;
+//		
+		System.out.println(s1.color);
+		System.out.println(s1.osVersion);
+		System.out.println(s2.osVersion);
+				
+		Smartphone.update();
 		
 	}
-
+	
 }
---
+++ src/de/demmer/dennis/Smartphone.java
@@ -0,0 +1,23 @@
+package de.demmer.dennis;
+
+public class Smartphone {
+	
+	String model;
+	String color;
+	
+	static int osVersion;
+	
+	
+	public static void update() {
+		//do something
+		
+		
+		//hochzählen der versionsnummer
+		
+//		osVersion++;
+		osVersion = osVersion +1;
+		
+	}
+	
+	
+}
